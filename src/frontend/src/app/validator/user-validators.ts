9ff83c40081adaace32b69b0019e608c9ce76a15
Alex <aspolale@gmail.com>
2020-04-04 17:00:23 +0100

    Added register form on front end Base validators added - password must match, required fields /etc.
    need to implement backend API's to check if email and username already exists on DB
    
    Added base model for user (subject to change)

---
+++ src/frontend/src/app/validator/user-validators.ts
@@ -0,0 +1,85 @@
+import {AbstractControl, AsyncValidatorFn} from "@angular/forms";
+import {environment} from "../../environments/environment";
+import {map, switchMap} from "rxjs/operators";
+import {Observable, timer} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserValidators {
+
+  constructor(private http: HttpClient) {
+  }
+
+  searchUsername(text) {
+    text = text.toLowerCase();
+    // debounce
+    return timer(200)
+      .pipe(
+        switchMap(() => {
+          // Check if username is available
+          return this.http.get<any>(`${environment.API_URL}/api/auth/checkusername/${text}`)
+        })
+      );
+  }
+
+  usernameValidator(): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<{ [key: string]: any } | null> => {
+      return this.searchUsername(control.value)
+        .pipe(
+          map(res => {
+            // if username is already taken
+            if (res.length) {
+              // return error
+              return { 'usernameExists': true};
+            }
+          })
+        );
+    };
+  }
+
+  searchUserEmail(text) {
+    text = text.toLowerCase();
+    // debounce
+    return timer(200)
+      .pipe(
+        switchMap(() => {
+          // Check if username is available
+          return this.http.get<any>(`${environment.API_URL}/api/auth/checkemail/${text}`)
+        })
+      );
+  }
+
+  emailValidator(): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<{ [key: string]: any } | null> => {
+      return this.searchUserEmail(control.value)
+        .pipe(
+          map(res => {
+            // if username is already taken
+            if (res.length) {
+              // return error
+              return { 'emailExists': true};
+            }
+          })
+        );
+    };
+  }
+
+
+
+
+  static MatchPassword(AC: AbstractControl) {
+    let password = AC.get('password').value;
+    if(AC.get('confirmPassword').touched || AC.get('confirmPassword').dirty) {
+      let verifyPassword = AC.get('confirmPassword').value;
+
+      if(password != verifyPassword) {
+        AC.get('confirmPassword').setErrors( {MatchPassword: true} )
+      } else {
+        return null
+      }
+    }
+  }
+}
