97f69389b5061bcbffd5d5672d5d33c5ea7ccab8
Alex <51273561+AlexanderBellDev@users.noreply.github.com>
2020-05-28 17:13:59 +0100

    Added user service user details patched into user form

--- src/frontend/src/app/editprofile/editprofile.component.ts
+++ src/frontend/src/app/editprofile/editprofile.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
+import {UserService} from "../service/user.service";
+import {User} from "../model/user";
 
 @Component({
   selector: 'app-editprofile',
@@ -7,19 +9,32 @@ import {FormBuilder, Validators} from "@angular/forms";
   styleUrls: ['./editprofile.component.css']
 })
 export class EditprofileComponent implements OnInit {
+  userDetails: User;
+
   userForm = this.formBuilder.group({
     id: [''],
-    firstName: ['Alex', [Validators.required]],
-    lastName: ['Test', [Validators.required]],
-    username: ['alex1234', [Validators.required]],
+    firstName: ['', [Validators.required]],
+    lastName: ['', [Validators.required]],
+    username: ['', [Validators.required]],
     password: ['', Validators.required]
   })
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private userService: UserService) {
   }
 
   ngOnInit(): void {
-
+    this.userService.getUserDetails().subscribe(value => {
+      if (value != null) {
+        this.userForm.patchValue({
+          id: value.id,
+          firstName: value.firstName,
+          lastName: value.surname,
+          username: value.username
+        });
+      }
+    }, error => {
+      console.log(error)
+    })
   }
 
 }
